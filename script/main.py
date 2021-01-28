aeb3e40d8df455bd0810390be093d0a72ce9b685
Brig Lamoreaux <bklamoreaux@gmail.com>
2021-01-27 20:43:08 -0700

    Add logger

--- script/main.py
+++ script/main.py
@@ -1,5 +1,23 @@
+#!/usr/bin/python
 """Main script for provision analytics workspaces."""
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+def configure_logger():
+    """Configure logger"""
+    _LOGGER.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+    _LOGGER.addHandler(handler)
 
 if __name__ == "__main__":
+    configure_logger()
+    _LOGGER.info("starting")
 
     print("hello world")
+
+    _LOGGER.info("done")
